a36e5bb5db27fa8f901d6e8ab1d363ecd4097f5b
ViralSir <viralsir2018@gmail.com>
2024-12-20 11:47:51 +0530

    for control has been completed.

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -9,29 +9,28 @@ import { Component } from '@angular/core';
 })
 export class StudentComponent
 {
-    rollno:number=0;
-    name:string="";
-    maths:number=0;
-    science:number=0;
-    english:number=0;
-    total:number=0;
-    pass_fail:string="";
+
+  StudentList:any=[];
+
+    // rollno:number=0;
+    // name:string="";
+    // maths:number=0;
+    // science:number=0;
+    // english:number=0;
+    // total:number=0;
+    // pass_fail:string="";
 
 
   Save(rollno: HTMLInputElement, name: HTMLInputElement, maths: HTMLInputElement, science: HTMLInputElement, english: HTMLInputElement) {
 
-    this.rollno=parseInt(rollno.value);
-    this.name=name.value;
-    this.maths=parseInt(maths.value);
-    this.science=parseInt(science.value);
-    this.english=parseInt(english.value)
-    if(this.maths>35 && this.science>35 && this.english>35){
-      this.pass_fail="Pass";
-      this.total=this.maths+this.science+this.english;
-    }
-    else{
-      this.pass_fail="Fail";
-    }
+        let student={
+           rno:rollno.value,
+           name:name.value,
+           maths:maths.value,
+           science:science.value,
+           english:english.value
+        }
+        this.StudentList.push(student);
 
-  }
+    }
 }
